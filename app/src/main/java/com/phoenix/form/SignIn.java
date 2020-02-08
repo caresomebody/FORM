08a3c0e3ab4774246d0983351fcbabd347a56858
Caresomebody <ali_naufal26@apps.ipb.ac.id>
2020-02-08 17:55:58 +0700

    merge branch 'master' of https://github.com/caresomebody/FORM.git

--- app/src/main/java/com/phoenix/form/SignIn.java
+++ app/src/main/java/com/phoenix/form/SignIn.java
@@ -1,13 +1,30 @@
 package com.phoenix.form;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignIn extends AppCompatActivity {
+
+    Button signIn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        signIn2= (Button) findViewById(R.id.SignIn2);
+
+        signIn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signIn2= new Intent(SignIn.this,Main.class);
+                startActivity(signIn2);
+            }
+        });
+
+
     }
 }
