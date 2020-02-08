8b8d3444fcd292c9fb39cc55258cf17d24a50db3
Caresomebody <ali_naufal26@apps.ipb.ac.id>
2020-02-08 12:42:32 +0700

    awal

---
+++ app/src/main/java/com/phoenix/form/MainActivity.java
@@ -0,0 +1,24 @@
+package com.phoenix.form;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private int waktu_loading=1500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent onboard= new Intent(getApplicationContext(),OnboardActivity.class);
+                startActivity(onboard);
+                finish();
+            }
+        },waktu_loading);
+    }
+}
